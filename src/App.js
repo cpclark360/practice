5be3fca15f32c0ef93b678682541f970956c042a
cpclark360 <40728556+cpclark360@users.noreply.github.com>
2019-05-20 11:04:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -68,6 +68,32 @@ const planets = [
     url: 'img/neptune.jpg' 
   },
 ];
+const Planet = () => {
+    return (
+      { planets.map(( worlds ) => 
+        <div class="container"> 
+        <div class="card">
+          <div>
+            <img src="img/mercury.jpg" alt="Mercury">
+          </div>
+          <h2>Mercury</h2>
+          <p>Mercury is the closest planet to the Sun. Due to its proximity, it's not easily seen except during twilight...</p>
+          <h3>Planet Profile</h3>
+          <ul>
+            <li><strong>Diameter:</strong> 3,031.67 mi</li>
+            <li><strong>Moons:</strong> none</li>
+          </ul>
+        </div>
+        <div class="card">...</div>
+      </div>  
+
+        )};
+    );
+}
+
+
+
+      
 
 
 const App = () => {
